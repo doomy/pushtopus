74abe5ddf6a7adc9fa217a3a690012f304d99e26
vladimir.bartek@gmail.com <vladimir.bartek@gmail.com>
2022-01-27 19:08:33 +0100

    Add public method isSubscribed to js pushtopus class

--- js/pushtopus.js
+++ js/pushtopus.js
@@ -124,6 +124,10 @@ function Pushtopus (
             });
     }
 
+    this.isSubscribed = function () {
+        return subscribed;
+    };
+
     function urlB64ToUint8Array(base64String) {
         const padding = '='.repeat((4 - base64String.length % 4) % 4);
         const base64 = (base64String + padding)
